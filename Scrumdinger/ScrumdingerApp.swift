3c92fd40c7905f83ab2d78fc1bd716200259c8a8
LutLastic Fantastic <50748557+LutLas@users.noreply.github.com>
2022-11-08 15:58:06 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Lutaka Muyoba Chihota on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
